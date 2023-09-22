998a0f6d6039c0efe47ae2c3db8dc17ccec53d53
Leo <leo.battalora@gmail.com>
2023-09-22 15:33:04 -0400

    home page

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      fontFamily: {
+        // sans: ["Graphik", "sans-serif"],
+        serif: ["DM Serif Display", "serif"],
+      },
+      colors: {
+        isabelline: "#f2e9e4",
+        "pale-dogwood": "#c9ada7",
+        "rose-quartz": "#9a8c98",
+        "ultra-violet": "#4a4e69",
+        "space-cadet": "#22223b",
+      },
+    },
+  },
+  plugins: [],
+};
